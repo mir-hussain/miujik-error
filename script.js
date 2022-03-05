8ef601488d0df6877a872fba2f8dba26fa42ea45
mir-hussain <mirhussainmurtaza@gmail.com>
2022-03-05 20:47:39 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const elementById = (id) => {
+  document.getElementById(id);
+};
+
+const handleSearch = () => {
+  const keyword = elementById("keyword");
+  const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showArtists(data));
+};
+
+const showArtists = (data) => {
+  const artistContainer = elementById("artist");
+  data?.artists?.forEach((artist) => {
+    const div = document.createElement("div");
+    div.classList.add("artist-card");
+    div.innerHTML = `<div class="image-container">
+    <div class="image-container-inner">
+      <img
+        src="${artist.strArtistThumb}"
+        alt=""
+      />
+    </div>
+  </div>
+  <div class="info-container">
+    <h1>${artist.strArtist}</h1>
+    <p>Country: ${artist.strCountry}</p>
+    <p>Style: ${artist.strGenre}</p>
+  </div>
+  <button class="album-button">
+    <i class="fa-solid fa-compact-disc"></i>
+    <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+  </button>`;
+    artistContainer.appendChild(div);
+  });
+};
+
+const fetchAlbums = (id) => {
+  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  fetch(url)
+    .then((res) => res.JSON())
+    .then((data) => showAlbum(data));
+  const artistContainer = elementById("artists");
+  artistContainer.innerHTML = "";
+};
+
+const showAlbum = (data) => {
+  const albumContainer = elementById("albums");
+  album.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList.add("album");
+    div.innerHTML = `
+        <div class="album-image-container">
+          <img
+            src="${album.strAlbumThumb}"
+            alt=""
+          />
+        </div>
+        <div class="album-name">
+          <h3>${album.strAlbum}</h3>
+        </div>
+      `;
+
+    albumContainer.appendChild(div);
+  });
+};
